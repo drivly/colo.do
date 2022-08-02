267341e31bad50a0a358243e11f18a0873f7fc96
Nathan Clevenger <nateclev@gmail.com>
2022-08-02 04:05:51 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,20 @@
+export default {
+  fetch: async (req, env) => {
+    const { colo: workerColo, longitude, longitude, country, region, city, asn, asOrganization, metroCode, postalCode, clientTcpRtt: latency } = req.cf
+    const visitor = { longitude, longitude, country, region, city, asn, asOrganization, metroCode, postalCode, latency }
+    const locations = await fetch('https://speed.cloudflare.com/locations').then(res => res.json())
+    const start = new Date()
+    const { colo: doColo } = await env.COLO.get(env.COLO.idFromName(workerColo))
+                            .fetch('https://workers.cloudflare.com/cf.json').then(res => res.json())
+    const durableObjectLatency = new Date() - start
+    const workerLocation = locations.find(loc => loc == workerColo)
+    const durableObjectLocation = locations.find(loc => loc == doColo)
+    return new Response(JSON.stringify({ durableObjectLatency, workerLocation, durableObjectLocation, visitor }, null, 2))
+  }
+}
+
+export class Colo {
+  async fetch(req) {
+    return fetch(req)
+  }
+}
